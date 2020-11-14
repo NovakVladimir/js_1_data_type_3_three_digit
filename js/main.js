04eb110beb0ea709adad6a6c8c8d8e73097367b4
NovakVladimir <vladimir.novak.89@i.ua>
2020-11-14 15:51:31 +0200

    Add result p

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 function changeVisibility() {
     document.getElementById("resultImg").style.visibility = 'visible';
+    document.getElementById("result").innerHTML = "Yes, it has"
 }
 
 function changeVisibility_2() {
     document.getElementById("resultImg").style.visibility = 'hidden';
+    document.getElementById("result").innerHTML = "No, it hasn't"
 }
 
 function isEqual() {
